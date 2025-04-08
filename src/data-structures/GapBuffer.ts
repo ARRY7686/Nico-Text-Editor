5470c1e37eeae54ca795cc4381d2172479dfdd87
Ojas Maheshwari <ojasmaheshwari2006@gmail.com>
2025-04-08 19:18:44 +0530

    Fix unused class bug that stopped deployment

--- src/data-structures/GapBuffer.ts
+++ src/data-structures/GapBuffer.ts
@@ -1,72 +1,74 @@
-class GapBuffer {
-    buffer: string[] = Array(4).fill("");
-    start = 0;
-    end = 4;
-    n = 4;
+export class GapBuffer {
+  buffer: string[] = Array(4).fill("");
+  start = 0;
+  end = 4;
+  n = 4;
 
-    Expand(): void {
-        let new_buffer = Array(this.n * 2).fill("");
-        for(let i=0; i<this.start; i++){ 
-            new_buffer[i] = this.buffer[i];
-        }
-        for(let i=this.end; i<this.n; i++){
-            new_buffer[this.n + i] = this.buffer[i];
-        }
-        this.end += this.n;
-        this.n *= 2;
-        this.buffer = new_buffer;
+  Expand(): void {
+    let new_buffer = Array(this.n * 2).fill("");
+    for (let i = 0; i < this.start; i++) {
+      new_buffer[i] = this.buffer[i];
     }
+    for (let i = this.end; i < this.n; i++) {
+      new_buffer[this.n + i] = this.buffer[i];
+    }
+    this.end += this.n;
+    this.n *= 2;
+    this.buffer = new_buffer;
+  }
 
-    Insert(s:string) : void{
-        if(this.start == this.end){
-            this.Expand();
-        }
-        this.buffer[this.start++] = s;
+  Insert(s: string): void {
+    if (this.start == this.end) {
+      this.Expand();
     }
+    this.buffer[this.start++] = s;
+  }
 
-    Left(): void {
-        if (this.start === 0) return;
-        this.start--;
-        this.end--;
-        this.buffer[this.end] = this.buffer[this.start];
-    }
+  Left(): void {
+    if (this.start === 0) return;
+    this.start--;
+    this.end--;
+    this.buffer[this.end] = this.buffer[this.start];
+  }
 
-    Right(): void{
-        if(this.end == this.n) return;
-        this.buffer[this.start] = this.buffer[this.end];
-        this.start++; 
-        this.end++; 
-    }
+  Right(): void {
+    if (this.end == this.n) return;
+    this.buffer[this.start] = this.buffer[this.end];
+    this.start++;
+    this.end++;
+  }
 
-    MoveCursor(pos: number): void {
-        const contentLength = this.start + (this.n - this.end);
-        pos = Math.max(0, Math.min(pos, contentLength));
-        while (this.start > pos) this.Left();
-        while (this.start < pos) this.Right();
-    }
+  MoveCursor(pos: number): void {
+    const contentLength = this.start + (this.n - this.end);
+    pos = Math.max(0, Math.min(pos, contentLength));
+    while (this.start > pos) this.Left();
+    while (this.start < pos) this.Right();
+  }
 
-    Backspace() : void{
-        if(this.start)  this.start--;
-    }
+  Backspace(): void {
+    if (this.start) this.start--;
+  }
 
-    DeleteForward() : void{
-        if(this.end < this.n) this.end++;
-    }
-    
-    Length() : number{
-        return this.n - (this.end - this.start);
-    }
+  DeleteForward(): void {
+    if (this.end < this.n) this.end++;
+  }
 
-    Get(pos: number) : string{
-        console.assert(pos >= 0 && pos < this.Length(), "GapBuffer Error: Index out of bounds");
-        if(pos < this.start) return this.buffer[pos];
-        return this.buffer[pos + this.end - this.start];
-    }
+  Length(): number {
+    return this.n - (this.end - this.start);
+  }
 
-    GetText(): string {
-        const preGap = this.buffer.slice(0, this.start).join('');
-        const postGap = this.buffer.slice(this.end, this.n).join('');
-        return preGap + postGap;
-    }
-    
+  Get(pos: number): string {
+    console.assert(
+      pos >= 0 && pos < this.Length(),
+      "GapBuffer Error: Index out of bounds"
+    );
+    if (pos < this.start) return this.buffer[pos];
+    return this.buffer[pos + this.end - this.start];
+  }
+
+  GetText(): string {
+    const preGap = this.buffer.slice(0, this.start).join("");
+    const postGap = this.buffer.slice(this.end, this.n).join("");
+    return preGap + postGap;
+  }
 }
